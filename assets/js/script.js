1f06a237336f0ae4450f69ae57a80f8fdd48fc68
Atsuko Coffey <atsucoffey@gmail.com>
2024-05-14 20:08:52 +0000

    feat: Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });//button.addEventListener
+    }//forLoop
+});//document.addEventListener
+
 function runGame() {
 
 }
@@ -13,6 +31,7 @@ function calculateCorrectAnswer() {
 function incrementScore() {
 
 }
+
 function incrementWrongAnswer() {
 
 }
